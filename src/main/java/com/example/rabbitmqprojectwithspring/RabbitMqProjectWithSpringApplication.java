f3b0236e104818c2cff11ec4297c5d40881ae743
hayk <haykk2018@gmail.com>
2022-11-09 18:06:32 +0400

    now we can run sender and receiver simultaneously

--- src/main/java/com/example/rabbitmqprojectwithspring/RabbitMqProjectWithSpringApplication.java
+++ src/main/java/com/example/rabbitmqprojectwithspring/RabbitMqProjectWithSpringApplication.java
@@ -12,20 +12,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class RabbitMqProjectWithSpringApplication {
 
+    //it's a default profile(set from application.yml)
     @Profile("usage_message")
     @Bean
     public CommandLineRunner usage() {
         return args -> {
             System.out.println("This app uses Spring Profiles to cntrol its behavior.\n");
-                    System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+            System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
         };
     }
-
+    // if we run without 'usage_message'(default) profile go this code
     @Profile("!usage_message")
     @Bean
     public CommandLineRunner tutorial() {
         return new RabbitAmqpTutorialsRunner();
     }
+
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqProjectWithSpringApplication.class, args);
     }
